e60a4118b444ead9219611b579b896e67d08e9fc
VuyoSopa <vssopangisa@gmail.com>
2023-11-01 21:46:01 +0200

    update on products

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductsComponent } from './products/products.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  
+ { path: '', redirectTo: 'products', pathMatch: 'full'},
+
+ {path: 'products', component: ProductsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+
+
+}
