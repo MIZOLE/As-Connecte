25b8b1833be21a8fd04909fed26e694ebc7dcbfa
xolisaboso <bosoxolisa@gmail.com>
2023-11-02 15:06:03 +0200

    loadVoucher

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductsComponent } from './products/products.component';
+import { LoadvoucherComponent } from './loadvoucher/loadvoucher.component';
 
 const routes: Routes = [
   
  { path: '', redirectTo: 'products', pathMatch: 'full'},
 
  {path: 'products', component: ProductsComponent},
+ {path: 'loadvoucher', component: LoadvoucherComponent},
+
+
+ 
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductsComponent } from './products/products.component';
+import { LoadvoucherComponent } from './loadvoucher/loadvoucher.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductsComponent
+    ProductsComponent,
+    LoadvoucherComponent
   ],
   imports: [
     BrowserModule,
