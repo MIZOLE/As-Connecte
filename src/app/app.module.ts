4dd5a899624ee3fee6db5ec4403cda48f3dc5784
bigfisssh <eduadvtech@gmail.com>
2023-11-02 11:51:04 +0200

    Signup page done!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductsComponent } from './products/products.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductsComponent
+    ProductsComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
