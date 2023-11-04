f140fddd7e9c1392a675e5358bbd2a74ca805ac6
Luka <lukhanyo95@gmail.com>
2023-11-04 18:36:11 +0200

    add signin functions

--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -1,14 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ResellerService } from 'src/services/reseller.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html',
-  styleUrls: ['./signin.component.scss']
+  styleUrls: ['./signin.component.scss'],
 })
+export class SigninComponent implements OnInit {
+  constructor(
+    private _resellerService: ResellerService,
+    private _router: Router
+  ) {}
 
+  signInForm: FormGroup = new FormGroup({
+    email: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
+  });
 
+  ngOnInit(): void {}
 
-
-export class SigninComponent {
-
+  signIn() {
+    this._resellerService.signIn(this.signInForm.value);
+  }
 }
