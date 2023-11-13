8119aef1848397d0b414f58ed0f06902dcafb90c
Luka <lukhanyo95@gmail.com>
2023-11-13 15:13:18 +0200

    add logout button

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -5,22 +5,26 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
-export class DashboardComponent implements OnInit{
-
-  constructor (private _resellerService: ResellerService, private _router: Router) {}
-
-  
+export class DashboardComponent implements OnInit {
+  constructor(
+    private _resellerService: ResellerService,
+    private _router: Router
+  ) {}
 
   ngOnInit(): void {
     if (!this._resellerService.getIsAuthenticated()) {
-      this._router.navigate(['/'])
+      this._router.navigate(['/']);
     }
   }
   counter = 0;
 
   increment() {
     this.counter++;
+  }
+
+  logOut() {
+    this._resellerService.logout();
   }
 }
