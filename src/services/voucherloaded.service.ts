c4815708ddcc1b4259fc9d8f9e2a36233ce2d9c6
khwinana <austinkhwinana23@gmail.com>
2023-11-13 13:52:36 +0200

    remove snackbar

--- src/services/voucherloaded.service.ts
+++ src/services/voucherloaded.service.ts
@@ -10,17 +10,6 @@ export class VoucherloadedService {
     let message;
     if (voucher.length < 12 || (voucher.length > 12 && voucher.length > 0)) {
       message = 'The voucher must be 12 digits long!'
-  days = 0;
-  hours = 0;
-  minutes = 0;
-  seconds = 0;
-
-  inavlidVoucher(voucher: any) {
-    if (voucher.length > 0 && voucher.length < 12 || voucher.length > 12) {
-      this._snackBarService.openSnackBar(
-        'The voucher must be 12 digits long!',
-        'FAILED'
-      );
     } else if (voucher.length === 0) {
       message = 'Please enter the voucher number!'
     } else if (/[^\d]/.test(voucher)) {
