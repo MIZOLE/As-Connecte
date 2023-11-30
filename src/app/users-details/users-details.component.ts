d9d88ddbc555b8654b6fbb6e5a1879933b6a8943
Luka <lukhanyo95@gmail.com>
2023-11-30 13:51:16 +0200

    update user details

--- src/app/users-details/users-details.component.ts
+++ src/app/users-details/users-details.component.ts
@@ -1,21 +1,21 @@
-import { Component, OnInit, OnDestroy  } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription, interval } from 'rxjs';
 import { ResellerService } from 'src/services/reseller.service';
 
 @Component({
   selector: 'app-users-details',
   templateUrl: './users-details.component.html',
-  styleUrls: ['./users-details.component.scss']
+  styleUrls: ['./users-details.component.scss'],
 })
 export class UsersDetailsComponent implements OnInit, OnDestroy {
   private subscription!: Subscription;
   private minSubscription!: Subscription;
 
-  public dateNow = new Date()
+  public dateNow = new Date();
 
-  number = this._reseller.getNumber() 
+  number = this._reseller.getNumber();
 
-constructor (private _reseller: ResellerService) {}
+  constructor(private _reseller: ResellerService) {}
 
   date = new Date().toDateString().split(' ');
   getHours = new Date().getHours();
@@ -28,68 +28,103 @@ constructor (private _reseller: ResellerService) {}
   getSeconds1 = new Date().getSeconds();
 
   dDay = new Date(
-    `Nov ${parseInt(this.date[2]) + 1}, 2023 ${this.getHours}:${this.getMinutes}:${this.getSeconds}`
+    `Nov ${parseInt(this.date[2]) + 1}, 2023 ${this.getHours}:${
+      this.getMinutes
+    }:${this.getSeconds}`
   ).getTime();
 
   dMinutes = new Date(
-    `Nov ${parseInt(this.date[2])}, 2023 ${this.getHours}:${this.getMinutes+1}:${this.getSeconds+30}`
+    `Nov ${parseInt(this.date[2])}, 2023 ${this.getHours}:${
+      this.getMinutes + 2
+    }:${this.getSeconds}`
   ).getTime();
 
   milliSecondsInASecond = 1000;
   hoursInADay = 24;
   minutesInAnHour = 60;
-  SecondsInAMinute  = 60;
+  SecondsInAMinute = 60;
 
   public timeDifference: any;
-  public minutesDifference: any
+  public minutesDifference: any;
   public secondsToDday: any;
   public minutesToDday: any;
-  public hoursToDday:any;
+  public hoursToDday: any;
   public daysToDday: any;
 
   public secondsToDday1: any;
   public minutesToDday1: any;
-  public hoursToDday1:any;
+  public hoursToDday1: any;
   public daysToDday1: any;
 
-  private getTimeDifference () {
-    this.timeDifference = this.dDay - new  Date().getTime();
+  private getTimeDifference() {
+    this.timeDifference = this.dDay - new Date().getTime();
     this.allocateTimeUnits(this.timeDifference);
-}
+  }
 
-private getMinutesDifference () {
-  this.minutesDifference = this.dMinutes - new  Date().getTime();
-  this.allocateMinutesUnits(this.minutesDifference);
-}
+  private getMinutesDifference() {
+    this.minutesDifference = this.dMinutes - new Date().getTime();
+    this.allocateMinutesUnits(this.minutesDifference);
+  }
 
-private allocateTimeUnits (timeDifference: any) {
-  this.secondsToDday = Math.floor((timeDifference) / (this.milliSecondsInASecond) % this.SecondsInAMinute);
-  this.minutesToDday = Math.floor((timeDifference) / (this.milliSecondsInASecond * this.minutesInAnHour) % this.SecondsInAMinute);
-  this.hoursToDday = Math.floor((timeDifference) / (this.milliSecondsInASecond * this.minutesInAnHour * this.SecondsInAMinute) % this.hoursInADay);
-  this.daysToDday = Math.floor((timeDifference) / (this.milliSecondsInASecond * this.minutesInAnHour * this.SecondsInAMinute * this.hoursInADay));
-}
+  private allocateTimeUnits(timeDifference: any) {
+    this.secondsToDday = Math.floor(
+      (timeDifference / this.milliSecondsInASecond) % this.SecondsInAMinute
+    );
+    this.minutesToDday = Math.floor(
+      (timeDifference / (this.milliSecondsInASecond * this.minutesInAnHour)) %
+        this.SecondsInAMinute
+    );
+    this.hoursToDday = Math.floor(
+      (timeDifference /
+        (this.milliSecondsInASecond *
+          this.minutesInAnHour *
+          this.SecondsInAMinute)) %
+        this.hoursInADay
+    );
+    this.daysToDday = Math.floor(
+      timeDifference /
+        (this.milliSecondsInASecond *
+          this.minutesInAnHour *
+          this.SecondsInAMinute *
+          this.hoursInADay)
+    );
+  }
 
-private allocateMinutesUnits (timeDifference: any) {
-  this.secondsToDday1 = Math.floor((timeDifference) / (this.milliSecondsInASecond) % this.SecondsInAMinute);
-  this.minutesToDday1 = Math.floor((timeDifference) / (this.milliSecondsInASecond * this.minutesInAnHour) % this.SecondsInAMinute);
-  this.hoursToDday1 = Math.floor((timeDifference) / (this.milliSecondsInASecond * this.minutesInAnHour * this.SecondsInAMinute) % this.hoursInADay);
-  this.daysToDday1 = Math.floor((timeDifference) / (this.milliSecondsInASecond * this.minutesInAnHour * this.SecondsInAMinute * this.hoursInADay));
-}
+  private allocateMinutesUnits(timeDifference: any) {
+    this.secondsToDday1 = Math.floor(
+      (timeDifference / this.milliSecondsInASecond) % this.SecondsInAMinute
+    );
+    this.minutesToDday1 = Math.floor(
+      (timeDifference / (this.milliSecondsInASecond * this.minutesInAnHour)) %
+        this.SecondsInAMinute
+    );
+    this.hoursToDday1 = Math.floor(
+      (timeDifference /
+        (this.milliSecondsInASecond *
+          this.minutesInAnHour *
+          this.SecondsInAMinute)) %
+        this.hoursInADay
+    );
+    this.daysToDday1 = Math.floor(
+      timeDifference /
+        (this.milliSecondsInASecond *
+          this.minutesInAnHour *
+          this.SecondsInAMinute *
+          this.hoursInADay)
+    );
+  }
 
-ngOnInit() {
-  this.subscription = interval(1000)
-      .subscribe(() => { this.getTimeDifference(); });
+  ngOnInit() {
+    this.subscription = interval(1000).subscribe(() => {
+      this.getTimeDifference();
+    });
 
-      this.minSubscription = interval(1000)
-      .subscribe(() => { this.getMinutesDifference(); });
+    this.minSubscription = interval(1000).subscribe(() => {
+      this.getMinutesDifference();
+    });
+  }
 
-      if(this.minutesToDday1 === 0 && this.secondsToDday1 === 0) {
-       this.number = 2
-      }
-
- }
- 
-ngOnDestroy() {
-this.subscription.unsubscribe();
-}
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
